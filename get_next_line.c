b00fef992c3e47d5ca7818d837be7808e5444a9e
Joao Cavadas Ribeiro <jcavadas@c2r2s7.42porto.com>
2024-04-17 14:27:40 +0100

    Started the projects logic

--- get_next_line.c
+++ get_next_line.c
@@ -6,14 +6,36 @@
 /*   By: jcavadas <jcavadas@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/05 20:43:41 by jcavadas          #+#    #+#             */
-/*   Updated: 2024/04/14 12:55:23 by jcavadas         ###   ########.fr       */
+/*   Updated: 2024/04/17 14:27:12 by jcavadas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char *get_next_line(int fd)
+int	read_from_fd(int fd, char **stash, char buffer)
 {
+	int	bytes;
+	
+	bytes = read(fd, buffer, BUFFER_SIZE);
 
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*stash;
+	char		*buffer;
+	int			bytes;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	buffer = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	while (ft_strchr(stash, '/n') == NULL && bytes > 0)
+		bytes = read_from_fd(fd, &stash, buffer);
+	free(buffer);
+	/*
+	vai estar a dar read do fd para o buffer, e vai passando para a stash (variavel estatica). vai ao mesmo tempo verificando se tem um /n no stash 
+	so precisa de estar a ir fazendo isto uma vez pois quando ele tiver um /n ja tem a linha para sair 
+	a funcao que vai ter la o read vai devolver o que o read devolve, que e um int, por isso depois vai se ter de verificar se devolveu -1 (erro do read) 
+	*/
 
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: jcavadas <jcavadas@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/05 21:04:13 by jcavadas          #+#    #+#             */
-/*   Updated: 2024/04/14 12:56:40 by jcavadas         ###   ########.fr       */
+/*   Updated: 2024/04/17 12:32:49 by jcavadas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,8 @@
 #  define BUFFER_SIZE 42
 # endif
 
-char *get_next_line(int fd); 
+char	*get_next_line(int fd);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char const *s1, char const *s2);
 
 #endif
